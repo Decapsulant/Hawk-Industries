5480baeb9ca6eef14cfbef52597490ca583034aa
Vyacheslav Petrenko <101598235+VyacheslavPetrenko@users.noreply.github.com>
2022-08-01 17:26:59 +0300

    first site

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header__menu').toggleClass('active')
+        $('body').toggleClass('lock')
+    })
+})
+
+/*smooth scroll*/
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors){
+  anchor.addEventListener('click', function(event){
+    event.preventDefault();
+    const blockId = anchor.getAttribute('href')
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior:'smooth',
+      block:'start'
+    })
+  })
+}
